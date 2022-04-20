601583cafa4d6752386be4bfa90f922d3ee15248
Michael Clement <michael.a.clement@erdc.dren.mil>
2022-04-20 11:25:11 -0500

    stubbed out component for point-cloud

---
+++ mppw/ui/vues/artifacts/digital-point-cloud-component.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="artifact">
+    <h1>Point-cloud component works!</h1>
+    {{ artifact }}<!-- raw JSON -->
+    <!--TODO: add text inputs here -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      artifact: null,
+    };
+  },
+  props: {
+    opId: String,
+    artifactId: String,
+  },
+  methods: {
+    refreshArtifact() {
+      this.artifact = null;
+      // root is API object (sorta). Replace "apiFetchArtifact" with 
+      // the proper API call.
+      // TODO: Get proper API call
+      return this.$root.apiFetchArtifact(this.artifactId).then((artifact) => {
+        this.artifact = artifact;
+      });
+    },
+  },
+  created() {
+    return this.refreshArtifact();
+  },
+};
+</script>
+
+<style scoped></style>
