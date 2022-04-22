ba2656add93715eba2f0ce88f0e8bc38092a5823
Michael Clement <michael.a.clement@erdc.dren.mil>
2022-04-22 16:35:13 -0500

    added download button for queried pointcloud data

--- mppw/ui/vues/artifacts/digital-point-cloud-component.vue
+++ mppw/ui/vues/artifacts/digital-point-cloud-component.vue
@@ -1,7 +1,17 @@
 <template>
   <div v-if="artifact">
-    <p>Point-cloud component works!</p>
-    <!-- {{ artifact }}raw JSON -->
+    <!--
+      TODO: 
+      - Remove JSON in top accordion?
+      - Improve date/time (use a picker)
+        - Maybe combo of calendar, clock, and text field (for milliseconds)
+        - Have a button that duplicates the min time so they don't have to
+          re-specify the date/hour for the upper bound?
+      - Improve input for space bounds
+      - Style fixes for JSON overflow
+      - Paginate the results using JS?? 
+        - Better yet, paginate at the API layer
+    -->
 
     <section>
       <o-collapse :open="false" class="card" animation="slide" style="margin-bottom: 5px;">
@@ -26,7 +36,7 @@
         <template #trigger="props">
           <div class="card-header" role="button" style="height: 40px;">
             <p class="card-header-title">
-              Fields 
+              Pull Point Cloud Data
             </p>
             <a class="card-header-icon">
               <o-icon :icon="props.open ? 'caret-up' : 'caret-down'"> </o-icon>
@@ -39,11 +49,14 @@
               <o-input v-model="artifact.local_data.id"></o-input>
             </o-field>
             <o-field label="space_bounds">
+              <!-- e.g.: [[-100000, -100000, -100000], [100000, 100000, 100000]] -->
               <o-input placeholder="[[x1, y1, z1], [x2, y2, z2]]"
                 v-model="artifact.local_data.space_bounds"></o-input>
             </o-field>
             <o-field label="time_bounds">
-              <o-input v-model="artifact.local_data.time_bounds"></o-input>
+              <!-- e.g.: ["2022-02-04T16:02:47.633000", "2022-02-04T16:02:47.90"] -->
+              <o-input placeholder="[ start_time , end_time ]"
+                v-model="artifact.local_data.time_bounds"></o-input>
             </o-field>
             <o-field label="coerce_dt_bounds">
               <o-checkbox v-model="artifact.local_data.coerce_dt_bounds">True</o-checkbox>
@@ -52,7 +65,15 @@
               <o-input v-model="artifact.local_data.format"></o-input>
             </o-field>
             <div class="mt-3 text-end">
-            <o-button @click="getPointCloud()" class="text-end" variant="primary">Get Point Cloud</o-button>
+            <o-button @click="getPointCloud()"
+              class="text-end">
+              Get Point Cloud
+            </o-button>
+            <o-button v-if="Object.keys(artifact.response).length > 0"
+              @click="downloadPointCloud(artifact.response)"
+              class="text-end btn-outline-secondary">
+                Download Point Cloud
+            </o-button>
             </div>
           </div>
         </div>
@@ -81,9 +102,6 @@ export default {
   methods: {
     refreshArtifact() {
       this.artifact = null;
-      // root is API object (sorta). Replace "apiFetchArtifact" with 
-      // the proper API call.
-      // TODO: Get proper API call
       return this.$root.apiFetchArtifact(this.artifactId).then((artifact) => {
         console.log(artifact);
         this.artifact = artifact;
@@ -112,6 +130,19 @@ export default {
           this.artifact.response = result;
       });
     },
+    downloadPointCloud(aft) {
+      // "aft" - artifact.response JSON object
+
+      // Stringify and format with 2 spaces for indentation
+      const data = JSON.stringify(aft, null, 2);
+      const blob = new Blob([data], {type: 'application/json'});
+      // Create an anchor tag, append the blob, and then click it to
+      // start the download
+      let a = document.createElement('a');
+      a.download = "point_cloud.json";
+      a.href = window.URL.createObjectURL(blob);
+      a.click();
+    }
   },
   created() {
     return this.refreshArtifact();
@@ -119,4 +150,10 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.o-btn.btn-outline-secondary {
+  color: initial;
+  background-color: white;
+  margin-left: 5px;
+}
+</style>
