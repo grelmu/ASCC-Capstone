45eea3dda8b8b607b0b79c42303d658ac5082c94
Nick Garfield <nicholas.garfield@maine.edu>
2022-05-09 09:33:32 -0400

    #9 transitioned everything over to Oruga components. Verified that each of the form components updates on the database end. Added some vue event handling to determine when things are edited.

---
+++ mppw/ui/vues/operation-metadata.vue
@@ -0,0 +1,86 @@
+<template>
+  <section v-if="metadata">
+    <h1>{{metadata['name']}}</h1>
+    <o-field grouped group-multiline label="Status and Tags">
+      <o-field>
+        <o-switch v-model="metadata['active']"  v-on:change="edit=true" >Active</o-switch>
+      </o-field><o-field>
+        <o-inputitems v-model="metadata['tags']" icon="tag" placeholder="Add tag"  v-on:add="edit=true" v-on:remove="edit=true" ></o-inputitems>
+      </o-field><o-field>
+        <o-input v-model="metadata['status']" placeholder="Status"  v-on:change="edit=true" ></o-input>
+      </o-field>
+    </o-field>
+    <o-field label="Description">
+      <o-input maxlength="500" type="textarea" v-model="metadata['description']"  v-on:change="edit=true" ></o-input>
+    </o-field>
+    <o-field label="System Name">
+      <o-input type="text" v-model="metadata['system_name']"  v-on:change="edit=true" ></o-input>
+    </o-field>
+    <o-field label="System ID">
+      <o-input type="text" v-model="metadata['system_id']"  v-on:change="edit=true" ></o-input>
+    </o-field>
+    <o-field label="Operator Names">
+      <o-inputitems v-model="metadata['human_operator_names']" icon="person" placeholder="Add an operator"  v-on:add="edit=true" v-on:remove="edit=true" ></o-inputitems>
+    </o-field>
+    <o-field label="Operator IDs">
+      <o-inputitems v-model="metadata['human_operator_ids']" icon="tag" placeholder="Add ID"  v-on:add="edit=true" v-on:remove="edit=true" ></o-inputitems>
+    </o-field>
+    <o-field label="Start Date & Time">
+      <o-input v-model="metadata['start_at']" icon="clock" type="datetime-local"  v-on:change="edit=true" ></o-input>
+    </o-field>
+    <o-field label="End Date & Time">
+      <o-input v-model="metadata['end_at']" icon="clock" type="datetime-local"  v-on:change="edit=true" ></o-input>
+    </o-field>
+    <o-field>
+    <o-button v-show="edit" @click="submit()">Submit</o-button>
+    </o-field>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      edit: null,
+    };
+  },
+  props: {
+    metadata: null,
+  },
+  methods: {
+    apiPutOp(opId,op) {
+      return this.$root
+        .apiFetch("operations/" + opId + "/", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(op),
+        })
+        .then((response) => {
+          if (response.status == 422) return response.json();
+          this.$root.throwApiResponseError(
+            response,
+            "Unknown response when saving operation"
+          );
+        });
+    },
+
+    submit(){
+      this.edit = false;
+      return this.apiPutOp(this.metadata['id'],this.metadata);
+    },
+  },
+  created() {
+    return true;
+  },
+};
+</script>
+
+<style scoped>
+.text-area {
+  width: 100%;
+  position: relative;
+  padding: 0.75rem;
+}
+</style>
