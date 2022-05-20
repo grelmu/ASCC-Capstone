2184d578fb261494096b1bb1f17738f560f8efff
Broderick, Sage <sage.broderick@erdc.dren.mil>
2022-05-20 05:16:32 -0500

    add check for sort_dir

--- mppw/operations.py
+++ mppw/operations.py
@@ -129,7 +129,8 @@ def create_router(app):
 
         # MongoDB's sort function expects either 1 or -1
         #   Convert sort_dir to match
-        sort_dir = 1 if sort_dir == 'asc' else -1
+        if sort_dir is not None:
+            sort_dir = 1 if sort_dir == 'asc' else -1
 
         results, total = repo_layer.operations.paged_query(
             project_ids=project_ids,
