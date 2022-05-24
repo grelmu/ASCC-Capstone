fa5476fe31df167bd6d2382be9343fba0369a443
Greg Studer <gregory.studer@maine.edu>
2022-05-23 21:38:59 -0400

    Default samples for tensile tests

--- mppw/services/operations/characterize_tensile_test_services.py
+++ mppw/services/operations/characterize_tensile_test_services.py
@@ -1,3 +1,4 @@
+from pyparsing import Char
 from .. import OperationServices
 from .. import ServiceLayer
 
@@ -5,6 +6,9 @@ from ... import models
 
 
 class CharacterizeTensileTestServices(OperationServices):
+
+    SAMPLE_KIND_PATH = [":sample"]
+
     def init(
         self,
         operation: models.Operation,
@@ -16,14 +20,17 @@ class CharacterizeTensileTestServices(OperationServices):
 
             sample_artifact = self._init_sample_artifact(operation)
             self.attach(
-                operation, [":sample"], sample_artifact.id, models.AttachmentMode.OUTPUT
+                operation,
+                models.AttachmentGraph.AttachmentNode.build(
+                    [":sample"], sample_artifact.id, models.AttachmentMode.OUTPUT
+                ),
             )
 
         return operation
 
     def _init_sample_artifact(self, operation):
 
-        sample_artifact = models.DigitalArtifact(
+        sample_artifact = models.MaterialArtifact(
             type_urn=models.MaterialArtifact.URN_PREFIX + ":sample",
             project=operation.project,
             name="Test Sample",
@@ -31,4 +38,19 @@ class CharacterizeTensileTestServices(OperationServices):
         )
 
         sample_artifact = self.repo_layer.artifacts.create(sample_artifact)
-        return ServiceLayer.artifact_services_for(sample_artifact).init(sample_artifact)
+        return self.service_layer.artifact_services_for(sample_artifact).init(
+            sample_artifact
+        )
+
+    def get_default_sample_node(self, operation: models.Operation):
+        return operation.attachments.find_node_by_path(
+            CharacterizeTensileTestServices.SAMPLE_KIND_PATH
+        )
+
+    def get_default_sample_artifact(self, operation):
+
+        sample_node = self.get_default_sample_node(operation)
+        if sample_node is None:
+            return None
+
+        return self.repo_layer.artifacts.query_one(id=sample_node.artifact_id)
