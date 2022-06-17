d0848be060522ea253a31b08d6d57b8ee1369351
Michael Clement <michael.a.clement@erdc.dren.mil>
2022-06-17 11:08:44 -0500

    Updated pointcloud interface to support chunk downloads (currently 2 minute increments)

--- mppw/ui/vues/artifacts/digital-point-cloud-component.vue
+++ mppw/ui/vues/artifacts/digital-point-cloud-component.vue
@@ -15,21 +15,11 @@
         <div class="card-content col-6">
           <div class="content" style="padding: 20px;">
             <o-field label="space_bounds">
-              <!--
-                TODO:
-                  Change placeholder text to show upper/lower bounds
-                  once it's added to the API endpoint
-              -->
               <!-- e.g.: [[-100000, -100000, -100000], [100000, 100000, 100000]] -->
               <o-input placeholder="[[x1, y1, z1], [x2, y2, z2]]"
                 v-model="formData.space_bounds"></o-input>
             </o-field>
             <o-field label="time_bounds_start">
-              <!--
-                TODO:
-                  Change placeholder text to show upper/lower bounds
-                  once it's added to the API endpoint
-              -->
               <!-- e.g.: ["2022-02-04T16:02:47.633000", "2022-02-04T16:02:47.90"] -->
               <o-datetimepicker
                 rounded placeholder="Click to select..." :timepicker="{ enableSeconds, hourFormat }"
@@ -42,17 +32,40 @@
                 icon="calendar" v-model="timeBoundsEnd" @update:modelValue="normalizeTimeBounds">
               </o-datetimepicker>
             </o-field>
-            <div class="mt-3 text-end">
-            <o-button @click="getPointCloud()"
-              class="text-end">
-              Get Point Cloud
-            </o-button><br/>
-            <o-button
-              @click="downloadPointCloud()"
-              class="text-end"
-              variant="info">
-                Download Point Cloud
-            </o-button>
+            <div class="mt-3 text-end pcl-btns">
+              <o-button @click="getPointCloud()"
+                class="text-end">
+                Get Point Cloud
+              </o-button><br/>
+              <o-button
+                @click="downloadPointCloud()"
+                class="text-end"
+                variant="info">
+                  Download Full Point Cloud
+              </o-button>
+            </div>
+
+            <div class="mt-3 text-end pcl-btns scrolling-btn-row">
+              <o-button v-if="tbChunks.length > 0"
+                id="l-scroll-btn" @click="scb(-300)">←</o-button>
+              <div v-for="(chunk, index) in tbChunks"
+                class="text-end imposter-btn o-btn"
+                outlined
+                :key="chunk[0]">
+                <div class="chunk-btn-ctr">
+                  <o-button inverted @click="(e) => {
+                      downloadPointCloud(e, chunk, `pc_chunk_${index}`)
+                    }"><o-icon :icon="'download'"></o-icon>
+                  </o-button>
+                  <o-button inverted @click="(e) => {
+                      gpc(e, chunk)
+                    }"><o-icon :icon="'eye'"></o-icon>
+                  </o-button>
+                </div>
+                <p>Chunk {{index}}</p>
+              </div><br/>
+              <o-button v-if="tbChunks.length > 0"
+                id="r-scroll-btn" @click="scb(300)">→</o-button>
             </div>
           </div>
         </div>
@@ -62,7 +75,18 @@
             TODO: 
               Paginate the results for large data (once API supports this)
           -->
-          <pre class="col-12">{{ JSON.stringify(response, null, 2) }}</pre>
+          <pre v-if="response.length < 5000" class="col-12">
+            {{ JSON.stringify(response, null, 2) }}
+          </pre>
+          <div class="large-warning" v-else-if="response.length >= 5000"> 
+            <p>
+              Large response &lpar;{{response.length}} items&rpar;
+              not displayed for page performance.
+              <br />
+              <br />
+              <b>Please download to view data.</b>
+            </p>
+          </div>
         </div>
 
       </o-collapse>
@@ -86,7 +110,8 @@ export default {
       locale: undefined,
       timeBoundsStart: null,
       timeBoundsEnd: null,
-      response: null
+      response: null,
+      tbChunks: [] 
     };
   },
   props: {
@@ -122,37 +147,148 @@ export default {
         });
     },
     normalizeTimeBounds() {
+      this.formData.time_bounds = this.normTB(
+        {
+          "start": this.timeBoundsStart,
+          "end": this.timeBoundsEnd
+        }
+      )
+    },
+    normTB(tb) {
       /*
         toISOString() results in Zulu time, which is +5 from New York.
         We reapply the timezone offset between the user's timezone and Zulu 
         so that what the user enters is what is queried on the backend.
       */
-      this.formData.time_bounds = JSON.stringify(
+      return JSON.stringify(
         [
-          new Date(this.timeBoundsStart.getTime() - (this.timeBoundsStart.getTimezoneOffset() * 60 * 1000)).toISOString(),
-          new Date(this.timeBoundsEnd.getTime() - (this.timeBoundsEnd.getTimezoneOffset() * 60 * 1000)).toISOString()
+          new Date(tb["start"].getTime() - (tb["start"].getTimezoneOffset() * 60 * 1000)).toISOString(),
+          new Date(tb["end"].getTime() - (tb["end"].getTimezoneOffset() * 60 * 1000)).toISOString()
         ]
       );
     },
     getPointCloud() {
-      
+      // Clear style on previously clicked chunk buttons:
+      document.querySelectorAll('.clicked-dl-btn').forEach(item => {item.classList.remove('clicked-dl-btn')})
       return this.$root.apiFetchPointcloud(
         this.artifactId,
         this.formData).then((result) => {
           this.response = result;
+
+          this.tbChunks = this.getDateChunks(
+            new Date(this.timeBoundsStart),
+            new Date(this.timeBoundsEnd),
+            // This means chunks are 2 minutes long: 
+            2 
+            // ^ TODO: swap to 1 hour chunks
+          );
+      });
+    },
+    // get point cloud, but for a chunk not the whole range 
+    // TODO: rename
+    gpc(e, chunk) {
+      // Update styling so the current chunk button is visually selected
+      document.querySelectorAll('.selected-chunk').forEach(el =>{el.classList.remove('selected-chunk')});
+      let targ = e.target;
+      while (targ.classList.contains('imposter-btn') == false) {
+        targ = targ.parentElement;
+      }
+      targ.classList.add("selected-chunk");
+
+      // Query for the chunk's data and display in the pane on the right
+      this.$root.apiFetchPointcloud(this.artifactId, 
+      {
+        space_bounds: this.formData.space_bounds,
+        time_bounds: this.normTB(chunk),
+        coerce_dt_bounds: this.formData.coerce_dt_bounds
+      }).then(res => {
+        this.response = res;
       });
     },
     /**
      * This function redirects to a download link to the points specified by the query
      */
-    downloadPointCloud() {
+    downloadPointCloud(e = null, chunk = null, fname = "point_cloud") {
+      let url;
 
       // Create an anchor tag and then click it to start the download
       let a = document.createElement('a');
-      a.download = "point_cloud.json";
-      a.href = this.$root.apiUrl(this.$root.apiFetchPointcloudUrl(this.artifactId, this.formData));
+      a.download = `${fname}.json`;
+
+      if (chunk == null) {
+        // We clicked the full download button, not a specific chunk
+        url = this.$root.apiUrl(this.$root.apiFetchPointcloudUrl(this.artifactId, this.formData));
+      } else {
+        url = this.buildIntervalURL(e, chunk);
+      }
+      console.log("Download URL: ", url);
+      a.href = url;
       a.click();
-    }
+    },
+    /**
+     * Divide a specified time range into n smaller [start, end] pairs
+     * based on a given subset size (i.e. 2 minute subsets)
+     * 
+     * Parameters:
+     * - start: the start time of the full range
+     * - end: the end time of the full range
+     * - chunkSize: the size of each smaller constituent time range
+     * 
+     * Returns:
+     * - an array of {start: Date(), end: Date()} objects representing
+     *   constituent chunks of the parent range
+     */
+    getDateChunks(start, end, chunkSize) {
+      // chunkSize is how big each chunk should be, i.e. 1 hour
+      // currently, it's in minutes. 
+      // TODO: Change to hours
+      let result = [];
+      let s = new Date(start);
+      // TODO: Make sure we're inclusive of the last data if chunk
+      // would extend beyond 'end' (so we don't lose last chunk of data)
+      while (s < end) {
+        let e = new Date(s);
+        e.setMinutes(e.getMinutes() + chunkSize);
+        result.push({start:new Date(s), end: e <= end? e : new Date(end)});
+        s.setMinutes(s.getMinutes() + chunkSize + 1); 
+      }
+      return result;
+    },
+    /**
+     * Creates a URL to download point cloud for a specified time range.
+     * Parameters:
+     * - chunk: an object with a start time and end time e.g:
+     *          {start: Date(), end: Date()}
+     * Returns:
+     * - url string usable to download a subset of the overall pointcloud
+     */
+    buildIntervalURL(e, chunk) {
+      let targ = e.target;
+      // doing this because if the user clicks on the text inside
+      // the button the event target isn't the button. TODO: make better.
+      while (targ.classList.contains('imposter-btn') == false) {
+        targ = targ.parentElement;
+      }
+      targ.classList.add('clicked-dl-btn');
+
+      let url = this.$root.apiUrl(
+        this.$root.apiFetchPointcloudUrl(this.artifactId, 
+        {
+          space_bounds: this.formData.space_bounds,
+          time_bounds: this.normTB(chunk),
+          coerce_dt_bounds: this.formData.coerce_dt_bounds
+        })
+      );
+      return url;
+    },
+    // Scroll Chunk Button function
+    scb(x) {
+      document.querySelector('.scrolling-btn-row').scrollBy({
+        top: 0,
+        left: x,
+        behavior: 'smooth'
+      });
+    },
   },
   created() {
     return this.refreshArtifact();
@@ -175,4 +311,95 @@ export default {
   margin-right: 10px;
   height: 100%;
 }
+.pcl-btns {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+}
+.pcl-btns button.o-btn {
+  margin-left: 10px;
+}
+.large-warning {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(235, 235, 235);
+  color: rgb(56, 56, 56);
+}
+.large-warning p {
+  margin: 25px;
+}
+.scrolling-btn-row {
+  position: relative;
+  max-width: 100%;
+  height: 70px;
+  overflow: hidden;
+  overflow-x: scroll;
+  justify-content: initial;
+  align-items: center;
+  border-radius: 4px;
+}
+.imposter-btn {
+  margin: 0px 5px 0px 5px;
+  position: relative;
+  min-width: 130px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.imposter-btn p {
+  position: absolute;
+  margin: 0 auto;
+  width: 100%;
+  text-align: center;
+}
+.imposter-btn .chunk-btn-ctr {
+  opacity: 0%;
+}
+.imposter-btn:hover .chunk-btn-ctr {
+  opacity: 100%;
+}
+.chunk-btn-ctr {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #bababa;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.chunk-btn-ctr button {
+  z-index: 90;
+  margin-left: 0px !important;
+  width: 50%;
+  border-radius: 0px;
+}
+#l-scroll-btn, #r-scroll-btn {
+  z-index: 100;
+  box-shadow: 0px 0px 5px #c0c0c0;
+}
+#l-scroll-btn {
+  position: sticky; left: 0px;
+  margin-left: 0px;
+}
+#r-scroll-btn {position: sticky; right: 0px;}
+
+.scrolling-btn-row .clicked-dl-btn {
+  border-color: #c0c0c0 !important;
+  color: #c0c0c0 !important;
+  background-color: white;
+}
+.scrolling-btn-row .clicked-dl-btn:hover {
+  background-color: white !important;
+  border-color: #c0c0c0 !important;
+  color: #c0c0c0 !important;
+}
+.selected-chunk {
+  box-shadow: 0px 0px 5px black;
+}
 </style>
+
