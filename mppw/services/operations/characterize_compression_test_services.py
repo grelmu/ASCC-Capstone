3668e41595ecbcf9099e95b6435290b0861c11dc
Greg Studer <gregory.studer@composites.maine.edu>
2022-09-28 15:17:43 -0400

    Basic compression test artifacts

---
+++ mppw/services/operations/characterize_compression_test_services.py
@@ -0,0 +1,10 @@
+from .. import OperationServices
+from .. import ServiceLayer
+
+from ... import models
+
+from .characterize_tensile_test_services import CharacterizeTensileTestServices
+
+
+class CharacterizeCompressionTestServices(CharacterizeTensileTestServices):
+    pass
