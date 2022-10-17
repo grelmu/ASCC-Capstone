a801dc6e0daa742226195acca75756df710c0e64
Greg Studer <gregory.studer@composites.maine.edu>
2022-10-17 13:50:46 -0400

    Always modify keyfile permissions on mdb container startup

--- containers/mppw-mongodb/50-init-root-password-keyfile.sh
+++ containers/mppw-mongodb/50-init-root-password-keyfile.sh
@@ -5,9 +5,10 @@ if [ ! -f "$ROOT_PASSWORD_KEYFILE" ]; then
   echo "Generating a new keyfile based on the root password..."
 
   echo "${MONGO_INITDB_ROOT_PASSWORD}" | md5sum | awk '{print $1}' > "$ROOT_PASSWORD_KEYFILE"
-  chmod 400 "$ROOT_PASSWORD_KEYFILE"
-  chown mongodb:root "$ROOT_PASSWORD_KEYFILE"
-
+  
   echo "  Done creating root password keyfile."
 
 fi
+
+chmod 400 "$ROOT_PASSWORD_KEYFILE"
+chown mongodb:root "$ROOT_PASSWORD_KEYFILE"
