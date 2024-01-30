8b66d896648b00f412e8a88f0d172f6691d90f04
Greg Studer <gregory.studer@composites.maine.edu>
2024-01-29 22:22:15 -0500

    Remove mppw

--- mppw/migrations/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def include_seed_data():
-    return (
-        os.environ.get("MPPW_MIGRATIONS_INCLUDE_SEED_DATA", "false").lower() == "true"
-    )
--- mppw/ui/vues/devtools-shim.js
+++
@@ -1,5 +0,0 @@
-function setupDevtoolsPlugin() {
-    // no-op   
-}
-
-export {setupDevtoolsPlugin}
